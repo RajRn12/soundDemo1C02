2cefc044083fc7efbab1b90236bf957b781a6820
Raj Rai <raj.rai@lethbridgecollege.ca>
2024-02-15 13:31:06 -0700

    Nice

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Audio } from 'expo-av';
 export default function App() {
-  return (
+    const [playbackStatus, setPlaybackStatus] = useState("Unloaded");
+
+    // load a sound
+
+    // play a sound
+
+    // pause a sound
+
+    // stop a sound
+
+    // unload a sound
+
+
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+          <Pressable
+              style={styles.button}
+              onPress={() => { } }
+          >
+              <Text style={styles.buttonText}>
+                  Play
+              </Text>
+          </Pressable>
+          <Pressable
+              style={styles.button}
+              onPress={() => { }}
+          >
+              <Text style={styles.buttonText}>
+                  Stop
+              </Text>
+            </Pressable>
+            <Text style={styles.buttonText}>Status: {playbackStatus}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +46,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
+    button: {
+        backgroundColor: 'lightgreen',
+        borderBlockColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderRadius: 7,
+        margin: 10,
+        padding: 10,
+    },
+    buttonText: {
+        fontSize: 24,
+    }
 });
